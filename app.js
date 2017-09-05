d7b5597c85613c727d1291ad8e9d2ea3059e2780
Jake Tripp <shmogens@gmail.com>
2017-09-05 14:44:13 -0500

    timer works, pause works, random color works, need to add way to change length of timer and break, need to add sounds after break and work

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ var lengthOfBreak = 5;
 var millisecondsPom = 1000 * 60 * lengthOfPomodoro;
 var timerCount = 2;
 
-var arr = $('h1').text();
+var arr = $('h2').text();
 arr = arr.split(':');
 var minutes = parseInt(arr[0]);
 var seconds = parseInt(arr[1]);
+
+var killInterval;
+var timerRunning = true;
+
 
 // ===================
 // AUXILIARY FUNCTIONS
@@ -57,29 +61,52 @@ function formatTime(seconds) {
     return str;
 }
 
+// [min,max]
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function updateFontColor(){
+    var r = getRandomInt(0,200);
+    var g = getRandomInt(0,200);
+    var b = getRandomInt(0,200);
+    var rgb = 'rgb(' + r + ',' + g + ',' + b + ')';
+    $('h1').css('color', rgb);
+    $('h2').css('color', rgb);
+    $('h3').css('color', rgb);
+    $('.ui.raised.segment').css('borderColor', rgb);
+}
+
+// ==============
+// MAIN FUNCTIONS
+// ==============
+
 function decreaseTimer(){
-	setInterval(function(){
+	killInterval = setInterval(function(){
+		timerRunning = true;
 		if (seconds === 0 && minutes === 0){
 			timerCount++;
 			handleBreakOrTimer();
-			console.log('minutes ' + minutes)
-			console.log('seconds ' + seconds)
 		}
 		if (seconds === 0 && minutes !== 0){
 			minutes--;
-			seconds = 59;
+			updateFontColor();
+			seconds = 60;
 		}
-		if (seconds !== 0) {
-			seconds--;
-		}
+		seconds--;
+
 		var str = minTwoDigits(minutes) + ':' + minTwoDigits(seconds);
-		$('h1').text(str);
+		$('h2').text(str);
 	}, 1000)
 }
 
 
-// BREAKTIME if even
-// TIMERTIME IF ODD
+// if even
+	// BREAKTIME 
+// else {
+	// TIMERTIME
 function isTimeForBreak(){
 	return (timerCount % 2 === 0);
 }
@@ -88,30 +115,52 @@ function handleBreakOrTimer(){
 	if (isTimeForBreak()){
 		console.log('time for break')
 		minutes = lengthOfBreak;
+		$('h3').text('BREAK TIME');
 	} else {
 		console.log('time for work')
 		minutes = lengthOfPomodoro;
+		$('h3').text('WORK TIME');
 	}
 	seconds = 0;
+}
+
+function bindEvents(){
+	$('.ui.raised.segment').on('click', function(){
+		if (timerRunning){
+			clearInterval(killInterval);
+			timerRunning = false;
+		} else {
+			decreaseTimer();
+		}
+	})
 }
 
 function updateDOM(){
 	decreaseTimer();
 }
 
-updateDOM();
+function init(){
+	updateDOM();
+	bindEvents();
+}
+
+init();
+
 
 
 // User Story: I can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
 
 // User Story: I can reset the clock for my next pomodoro.
+// on click, have the 25 minute timer start
+// have an animation so visually you can tell that the clock is going (and when the break is going)
+// have it be able to pause the timer or break on click
+// change to a random color (steal from as of today) every such period of time
+// every minute? every 10 seconds? every 5 seconds?
+
+
+
 
 // User Story: I can customize the length of each pomodoro.
-
-
-// on click, have the 25 minute timer start
-
-// have an animation so visually you can tell that the clock is going (and when the break is going)
 
 // after the timer goes to zero, play a sound, start the 5 break
 
@@ -119,7 +168,5 @@ updateDOM();
 // use a global variable to store it
 // use an input type number to change it
 // have event onChange to update the variable
-
-// have it be able to pause the timer or break on click
 
 
